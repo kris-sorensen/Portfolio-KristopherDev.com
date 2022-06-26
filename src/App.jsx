a644ddb88b20bce854c15934a355ad5843ec343e
Kris Sorensen <krismsorensen@gmail.com>
2022-06-25 21:05:20 -0600

    git changing to lowercase on build attempted fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import ContentContainer from './Components/ContentContainer'
+import ContentContainer from './components/ContentContainer'
 import CanvasContainer from './Components/CanvasContainer'
 
 function App() {
