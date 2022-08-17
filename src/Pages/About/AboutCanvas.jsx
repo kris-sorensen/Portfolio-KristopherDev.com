648766a5ee3b29a087641e05a6ddad58a4a4aaf9
Kris Sorensen <krismsorensen@gmail.com>
2022-08-16 18:45:12 -0600

    sound test 2

--- src/Pages/About/AboutCanvas.jsx
+++ src/Pages/About/AboutCanvas.jsx
@@ -10,7 +10,8 @@ import { useControls, Leva } from 'leva';
 // import PreExplodedFirework from './PreExplodedFirework'
 import { AfterimagePass } from "three-stdlib"
 import fireworkSound from './audio/firework.mp3';
-import fireworkSoundMobile from './audio/firework-shortest.mp3';
+// import fireworkSoundMobile from './audio/firework-shortest.mp3';
+import fireworkSoundMobile from './audio/firework-short.mp3';
 import useSound from 'use-sound';
 import useWindowSize from '../../hooks/useWindowSize';
 
