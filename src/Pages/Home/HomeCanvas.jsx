d82b2abcf0712354dc85d26a721345d96c4f2bf4
Kris Sorensen <krismsorensen@gmail.com>
2022-09-28 22:20:23 -0600

    lowered multisamping because getting a green color on mobile

--- src/Pages/Home/HomeCanvas.jsx
+++ src/Pages/Home/HomeCanvas.jsx
@@ -89,8 +89,9 @@ function HomeCanvas() {
                         <SemiTransparentLayer renderIndex={-2} opacity={transparentLayerParams.opacity} />
                         {/* <OrbitControls /> */}
                         {/* <PreExplodedFirework /> */}
-                        <Effects multisamping={10} renderIndex={-1} disableGamma={false} depthBuffer={true}>
-                            <afterimagePass args={[0]} />
+                        <Effects multisamping={0} renderIndex={-1}
+                        >
+                            <afterimagePass args={[0.6]} />
                         </Effects>
 
                         <HomeContent />
